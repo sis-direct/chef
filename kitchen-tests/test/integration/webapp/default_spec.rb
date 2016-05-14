39eb3fcd14a271a2265a9bbac2e3a5c9df8e4a3b
Lamont Granquist <lamont@scriptkiddie.org>
2016-05-13 17:10:24 -0700

    autofixing inspec

--- kitchen-tests/test/integration/webapp/default_spec.rb
+++ kitchen-tests/test/integration/webapp/default_spec.rb
@@ -7,77 +7,77 @@
 #  its("stdout") { should match /Hello, World!/ }
 #end
 
-describe package('nscd') do
+describe package("nscd") do
   it { should be_installed }
 end
 
-describe service('nscd') do
+describe service("nscd") do
 # broken?
 #  it { should be_enabled }
   it { should be_installed }
   it { should be_running }
 end
 
-describe package('fail2ban') do
+describe package("fail2ban") do
   it { should be_installed }
 end
 
-describe service('fail2ban') do
+describe service("fail2ban") do
   it { should be_enabled }
   it { should be_installed }
   it { should be_running }
 end
 
-describe package('openssh-client') do
+describe package("openssh-client") do
   it { should be_installed }
 end
 
-describe service('ssh') do
+describe service("ssh") do
   it { should be_enabled }
   it { should be_installed }
   it { should be_running }
 end
 
-describe package('ntp') do
+describe package("ntp") do
   it { should be_installed }
 end
 
-describe service('ntp') do
+describe service("ntp") do
 # broken?
 #  it { should be_enabled }
   it { should be_installed }
   it { should be_running }
 end
 
-describe package('chef') do
+describe package("chef") do
   it { should be_installed }
 end
 
-describe service('chef-client') do
+describe service("chef-client") do
   it { should be_enabled }
   it { should be_installed }
   it { should be_running }
 end
 
-describe file('/etc/resolv.conf') do
-  its('content') { should match /search\s+chef.io/ }
-  its('content') { should match /nameserver\s+8.8.8.8/ }
-  its('content') { should match /nameserver\s+8.8.4.4/ }
+describe file("/etc/resolv.conf") do
+  its("content") { should match /search\s+chef.io/ }
+  its("content") { should match /nameserver\s+8.8.8.8/ }
+  its("content") { should match /nameserver\s+8.8.4.4/ }
 end
 
-describe package('gcc') do
+describe package("gcc") do
   it { should be_installed }
 end
 
-describe package('flex') do
+describe package("flex") do
   it { should be_installed }
 end
 
-describe package('bison') do
+describe package("bison") do
   it { should be_installed }
 end
 
-describe package('autoconf') do
+describe package("autoconf") do
   it { should be_installed }
 end
 
@@ -87,26 +87,26 @@ end
   end
 end
 
-describe apt('http://us-west-2.ec2.archive.ubuntu.com/ubuntu') do
+describe apt("http://us-west-2.ec2.archive.ubuntu.com/ubuntu") do
   it { should be_enabled }
   it { should exist }
 end
 
-describe etc_group.where(group_name: 'sysadmin') do
-  its('users') { should include 'adam' }
-  its('gids') { should eq [2300] }
+describe etc_group.where(group_name: "sysadmin") do
+  its("users") { should include "adam" }
+  its("gids") { should eq [2300] }
 end
 
-describe passwd.filter(user: 'adam') do
-  its('uids') { should eq ["666"] }
+describe passwd.filter(user: "adam") do
+  its("uids") { should eq ["666"] }
 end
 
 describe ntp_conf do
-  its('server') { should_not eq nil }
+  its("server") { should_not eq nil }
 end
 
 describe port(22) do
   it { should be_listening }
-  its('protocols') { should include 'tcp' }
-  its('processes') { should eq ['sshd'] }
+  its("protocols") { should include "tcp" }
+  its("processes") { should eq ["sshd"] }
 end
